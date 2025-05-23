b5cb0eda727e809c5550674a43a919931a9d5069
Jhimmy <jhimmymontenegro97@gmail.com>
2025-05-23 12:23:11 -0500

    Modifica archivo .py para probar ejecucion my-event-workflow con PR

--- src/0-split-dataset.py
+++ src/0-split-dataset.py
@@ -2,7 +2,8 @@ import pandas as pd
 import fire
 import os
 
-#comentario de prueba para validar ejecucion con event-filter por parth
+#Comentario de prueba para validar ejecucion con event-filter por parth
+#Otro comentario de prueba
 
 def split_data(df_data, perc_data_train):
     df_data_train = df_data.sample(frac=perc_data_train)
